9e95efb64a6c0512e48892abdb4e955466f75cff
™iweed™ <44798066+1a-i-w-e-e-d@users.noreply.github.com>
2018-11-06 16:14:55 -0800

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -132,8 +132,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"your_seed_ip1.com:8080",
-  //"your_seed_ip2.com:8080",
+  //"iweed.pagekite.me:8080",
+  //"i-w-e-e-d.com:8080",
 };
 
 struct CheckpointData {
